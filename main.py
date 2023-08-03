f90323b62d6066134c0064f462f107b08965602b
Anthony Le <aznthonyle45@gmail.com>
2023-08-03 12:43:23 -0700

    Added progressbar and button with corresponding update function that prints a debug message of the event.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("The start of the most useless and frustrating loading bar. :D")
+from tkinter import *
+from tkinter.ttk import *
+
+# Updates the bar value
+def progress_bar_update():
+    print("Button Pressed")
+
+
+if __name__ == "__main__":
+    print("Start of the GUI")
+
+    root = Tk()
+
+    # Set fixed constraints and dimensions for the window
+    root.resizable(FALSE, FALSE)
+    root.geometry("600x300")
+    root.title("The Unsatisfactory Loading Bar")
+
+    # Load in a Loading Bar
+    prog = Progressbar(root, orient=HORIZONTAL, length=500, mode="determinate")
+
+    # Button to activate loading animation for bar
+    start_prog = Button(root, text='Start', command=progress_bar_update)
+
+    prog.place(relx=0.5, rely=0.5, anchor=CENTER)
+    start_prog.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    print("End of GUI")
+    root.mainloop()
+
